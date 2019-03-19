753883d97679db1f7fe7daad58e13dac2f73f627
msmrkovic1 <msmrkovic1@etf.unsa.ba>
2019-03-19 20:41:59 +0100

    dodaj metodu najvecaRijec

--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -6,10 +6,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner ulaz = new Scanner(System.in);
 
-        String s = "";
+        String str = "";
         System.out.println("Unesite jednu recenicu: ");
-        s = ulaz.nextLine();
+        str = ulaz.nextLine();
 
-        System.out.println(s);
+        //rastavlja recenicu
+        String[] strArr = str.split(" ");
+
+        //ispis promjenjene recenice
+        System.out.println(najvecaRijec(strArr));
+    }
+
+    private static String najvecaRijec(String[] strArr) {
+        //posljednja najduza rijec
+        String max = "";
+
+        //nalazi najvecu rijec
+        for(String item : strArr){
+            if(max.length()<item.length()) max=item;
+        }
+
+        String s = "";
+        //indeksiranje forloop-a
+        int i = 0;
+        for (String item : strArr) {
+            if (max.equals(item)) {
+                s += item.toUpperCase();
+            } else {
+                s += item;
+            }
+            if (i < strArr.length - 1) s += ' ';
+            i = i + 1;
+        }
+
+        return s;
     }
 }
