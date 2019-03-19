2463da8f05ebd10e222a7bd1d3cdd7d157a8306c
msmrkovic1 <msmrkovic1@etf.unsa.ba>
2019-03-19 17:29:15 +0100

    initial commit
    zaboravio napraviti repo na new file

---
+++ src/ba/unsa/etf/rs/Main.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rs;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner ulaz = new Scanner(System.in);
+
+        String s = "";
+        System.out.println("Unesite jednu recenicu: ");
+        s = ulaz.nextLine();
+
+        System.out.println(s);
+    }
+}
